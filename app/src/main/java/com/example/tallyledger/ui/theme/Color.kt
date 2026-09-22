f00208d464466faf7077bd125c247f1a31a048e9
Adi <ydvaditya20@gmail.com>
2026-09-22 13:39:58 +0530

    Upload content of folder firebase-project

---
+++ app/src/main/java/com/example/tallyledger/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package com.example.tallyledger.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Scheme Colors
+val PrimaryIndigo = Color(0xFF4F46E5)
+val OnPrimaryWhite = Color(0xFFFFFFFF)
+val PrimaryContainerIndigo = Color(0xFFE0E7FF)
+val OnPrimaryContainerIndigo = Color(0xFF312E81)
+
+val SecondarySky = Color(0xFF0284C7)
+val OnSecondaryWhite = Color(0xFFFFFFFF)
+val SecondaryContainerSky = Color(0xFFE0F2FE)
+val OnSecondaryContainerSky = Color(0xFF075985)
+
+val TertiaryEmerald = Color(0xFF16A34A)
+val OnTertiaryWhite = Color(0xFFFFFFFF)
+val TertiaryContainerEmerald = Color(0xFFDCFCE7)
+val OnTertiaryContainerEmerald = Color(0xFF166534)
+
+val BackgroundSlate = Color(0xFFF8FAFC)
+val OnBackgroundSlate = Color(0xFF0F172A)
+val SurfaceCard = Color(0xFFFFFFFF)
+val OnSurfaceCard = Color(0xFF1E293B)
+val SurfaceVariantSlate = Color(0xFFF1F5F9)
+val OnSurfaceVariantSlate = Color(0xFF475569)
+val OutlineSlate = Color(0xFFCBD5E1)
+
+val ErrorRed = Color(0xFFDC2626)
+val OnErrorWhite = Color(0xFFFFFFFF)
+val ErrorContainerRed = Color(0xFFFEE2E2)
+val OnErrorContainerRed = Color(0xFF991B1B)
+
+// Accounting specific tokens
+val DebitBlueText = Color(0xFF1E40AF)
+val DebitBlueBg = Color(0xFFDBEAFE)
+val CreditRedText = Color(0xFF991B1B)
+val CreditRedBg = Color(0xFFFEE2E2)
+val GoldVoucher = Color(0xFFD97706)
